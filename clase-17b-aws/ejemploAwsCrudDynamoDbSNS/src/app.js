899415007a1b581c8e59c98d5852e49bcc1e9f63
mariano aquino <mariano.aquino@gmail.com>
2023-01-04 16:56:31 -0300

    agrega material clase 34

---
+++ clase-17b-aws/ejemploAwsCrudDynamoDbSNS/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { productosRouter } = require("./productosRouter.js")
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => { res.send('test api') })
+
+app.use('/api/productos', productosRouter)
+
+const port = process.env.PORT || 8080
+const server = app.listen(port, () => {
+  console.log(`Escuchando en puerto ${server.address().port}`)
+})
+server.on('error', error => { console.log(error) })
